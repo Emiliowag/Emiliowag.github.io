593a4127f4944d316f7acc55167016cec72fb968
Emiliowag <emiliowag09@gmail.com>
2024-11-04 12:31:17 +0100

    a

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let manager = new GameStateManager();
+let c;
+
 function setup() {
-  createCanvas(400, 400);
+  document.body.style.overflow = "hidden";
+  c = createCanvas(415, 800);
+  textAlign(CENTER, BASELINE);
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  c.position(x, y);
+  manager.opretGameState("menu", new MenuSkærm());
+  manager.opretGameState("spil", new SpilSkærm());
+  manager.skiftGameState("menu");
+}
+function draw() {
+  manager.runState();
+}
+function mouseClicked() {
+  manager.aktuelleSkærm.musKlikket();
 }
 
-function draw() {
-  background(220);
+function keyReleased() {
+  if (key === " ") {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
+
+function windowResized() {
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  c.position(x, y);
+  manager = new GameStateManager();
+  manager.opretGameState("menu", new MenuSkærm());
+  manager.opretGameState("spil", new SpilSkærm());
+  manager.skiftGameState("menu");
 }
